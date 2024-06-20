dfd6aa466f888b0c2674f4b2b79590b7e8a186c1
Nirbhay Singh <90407590+NiRbHaYsInGh30@users.noreply.github.com>
2024-06-20 08:36:42 +0530

    Installed vercel dependecy

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
 import App from './app/App';
+import { Analytics } from "@vercel/analytics/react";
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <App />
+    
 );
