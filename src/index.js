38fa07da2268945260cecd96807437dde6ec1e93
Nirbhay Singh <90407590+NiRbHaYsInGh30@users.noreply.github.com>
2024-07-29 16:58:32 +0530

    Added vercel speed insights

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from './app/App';
 import { Analytics } from "@vercel/analytics/react";
 import './index.css';
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
     <Analytics />
+    <SpeedInsights />
   </React.StrictMode>
 );;
